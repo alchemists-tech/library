3565eca63b115d5e0cb29252a175babb0b8a7bb9
whall <winston.hall@kinsaleins.com>
2021-12-28 15:50:36 -0500

    js methods

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-function Book(title, author, pages) {
+let myLibrary = [];
+const addButton = document.querySelector(".add");
+
+function Book(title, author, pages, read = false) {
 
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = false;
+    this.read = read;
 
 };
 
+addButton.addEventListener(click, )
+
 Book.prototype.info = function() {
 
-    let phrase = ""
+    let phrase = "";
 
     if (!this.read) {
 
@@ -19,8 +24,14 @@ Book.prototype.info = function() {
 
         phrase = "has read already"
 
-    }
+    };
 
-    return `${title} by ${author}, ${pages} pages, ${phrase}.`
+    return `${title} by ${author}, ${pages} pages, ${phrase}.`;
 
-}
+};
+
+function addBookToLibrary(title, author, pages, read) {
+
+    myLibrary.push( new Book(title, author, pages, read));
+
+};
